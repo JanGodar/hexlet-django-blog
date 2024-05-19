149051abf709b69ad610f5401c0fa38893f3ad64
JanGodar <2017sofin@gmail.com>
2024-05-19 11:21:45 +0300

    step 6

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views import View
 # from django.http import HttpResponse
 
-def index(request):
-    return render(request, 'articles.html', context= {
-        'name': 'name_page'
-    })
+
+class IndexView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'articles.html', context= {
+            'name': 'name_page'
+        })
